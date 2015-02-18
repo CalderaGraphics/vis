96de6d4fc4ad97fe9c13d2aa84ac3a7e47900f51
Yannick Lohse <yannick.lohse@caldera.com>
2015-02-18 14:34:01 +0100

    custom build

---
+++ custom.js
@@ -0,0 +1,4 @@
+//browserify custom.js -o dist/vis-caldera.js -s vis -x moment 
+exports.DataSet = require('./lib/DataSet');
+exports.DataView = require('./lib/DataView');
+exports.Timeline = require('./lib/timeline/Timeline');
